de138eeb33ec83d0a2f5819778da355fcbcc56e3
enamulkh <33300969+enamulkh@users.noreply.github.com>
2018-01-31 16:47:04 -0600

    BOTRepoGitHub
    BOTRepoGitHub

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot-1-01-23-2018-Web-Deploy.pubxml -p:Password=HXmjiXxylZbxMfEGs3ZrBzFavKqAj8FQnuq6HxagDhaKyeDANcbx4yQpPkmu
+
